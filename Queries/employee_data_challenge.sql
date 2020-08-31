77e8ebd2d16eb7c3e664ffd41dbc6aa47a4356c5
jricebae19 <68259136+jricebae19@users.noreply.github.com>
2020-08-30 20:13:37 -0700

    forgot to add mentorship code to employee_data_challenge sql file

--- Queries/employee_data_challenge.sql
+++ Queries/employee_data_challenge.sql
@@ -20,9 +20,13 @@ FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY count DESC;
 
--- Retrieve number of retirees by title
-SELECT COUNT(ut.title), ut.title
-INTO retiring_titles
-FROM unique_titles AS ut
-GROUP BY ut.title
-ORDER BY count DESC;
+-- Creating Mentorship Eligiblity table for Deliverable 2
+SELECT DISTINCT ON (emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees AS e
+LEFT JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+LEFT JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31' AND de.to_date = ('9999-01-01'))
+ORDER BY emp_no;
